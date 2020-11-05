911224afa610e8a464db17130a10289dc541d023
andrew <fleming-andrew@protonmail.com>
2020-11-05 14:03:24 -0500

    started unit testing

--- test/DexquisiteCorpse.test.js
+++ test/DexquisiteCorpse.test.js
@@ -1,3 +1,5 @@
+const { default: Web3 } = require('web3')
+
 const DaiToken = artifacts.require('DaiToken')
 const DexquisiteToken = artifacts.require('DexquisiteToken')
 const DexquisiteCorpse = artifacts.require('DexquisiteCorpse') 
@@ -6,11 +8,29 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('DexquisiteCorpse', (accounts) => {
+function tokens(n) {
+   return web3.utils.toWei(n, 'ether')
+}
+
+contract('DexquisiteCorpse', ([owner, investor]) => {
+    let daiToken, dexquisiteToken, dexquisiteCorpse
+
+    //recreate what happens in migration
+    before( async() => {
+        daiToken = await DaiToken.new(tokens('100'))
+        dexquisiteToken = await DexquisiteToken.new(tokens('1000'))
+        dexquisiteCorpse = await DexquisiteCorpse.new(dexquisiteToken.address, daiToken.address)
+
+        //transfer dxq to dexquisite corpse farm
+        await dexquisiteToken.transfer(dexquisiteCorpse.address, tokens('1000'))
+
+        //transfer dai to investor
+        await daiToken.transfer(investor, tokens('100'), {from: owner})
+
+    })
 
     describe('Mock Dai deployment', async () => {
         it('has a name', async () => {
-            let daiToken = await DaiToken.new('100000000000000000000')
             const name = await daiToken.name()
             assert.equal(name, 'Dai')
         })
