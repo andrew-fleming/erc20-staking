08fea5c40022b8d629a57f850fe010565723f1a9
andrew <fleming-andrew@protonmail.com>
2020-11-06 03:48:32 -0500

    added staking, minting funcs in smart contract + tests

--- contracts/DexquisiteCorpse.sol
+++ contracts/DexquisiteCorpse.sol
@@ -4,14 +4,62 @@ import './DaiToken.sol';
 import './DexquisiteToken.sol';
 
 contract DexquisiteCorpse {
+    address public owner;
     string public name = 'Dexquisite Corpse';
 
-    //save state variables, declaring smart contracts as 'types'
     DexquisiteToken public dexquisiteToken;
     DaiToken public daiToken;
+
+    address[] public stakers;
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
 
     constructor(DexquisiteToken _dexquisiteToken, DaiToken _daiToken) public {
         dexquisiteToken = _dexquisiteToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
+
+
+    //stake tokens
+    function stake(uint256 _amount) public {
+        require(_amount > 0, 'You cannot stake zero tokens');
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[msg.sender] += _amount;
+        //if user hasn't staked, add them to array
+        if(!hasStaked[msg.sender]){
+            stakers.push(msg.sender);
+        }
+
+        hasStaked[msg.sender] = true;
+        isStaking[msg.sender] = true;
+    }
+
+
+    //unstake tokens
+    function unstake() public {
+        require(isStaking[msg.sender] = true, 'You are not staking tokens');
+        uint256 balance = stakingBalance[msg.sender];
+        stakingBalance[msg.sender] = 0;
+        daiToken.transferFrom(address(this), msg.sender, balance);
+        hasStaked[msg.sender] = false;
+    }
+
+    //issue tokens
+    function issueTokens() public onlyOwner{
+        for(uint i = 0; i < stakers.length; i++){
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if(balance > 0){
+                dexquisiteToken.transfer(recipient, balance);
+            }
+        }
+    }
+
 }
--- test/DexquisiteCorpse.test.js
+++ test/DexquisiteCorpse.test.js
@@ -1,3 +1,4 @@
+const { assert } = require('chai')
 const { default: Web3 } = require('web3')
 
 const DaiToken = artifacts.require('DaiToken')
@@ -33,6 +34,57 @@ contract('DexquisiteCorpse', ([owner, investor]) => {
         it('has a name', async () => {
             const name = await daiToken.name()
             assert.equal(name, 'Dai')
+        })
+    })
+
+    describe('Dexquisite Token deployment', async() => {
+        it('has a name', async () => {
+            const name = await dexquisiteToken.name()
+            assert.equal(name, 'Dexquisite')
+        })
+    })
+
+    describe('Dexquisite Corpse', async() => {
+        it('has a name', async() => {
+            const name = await dexquisiteCorpse.name()
+            assert.equal(name, 'Dexquisite Corpse')
+        })
+
+        it('contract has tokens', async() => {
+            const balance = await dexquisiteToken.balanceOf(dexquisiteCorpse.address)
+            assert.equal(balance.toString(), tokens('1000'))
+        })
+    })
+
+    describe('Farming tokens', async() => {
+        it('rewards investors for staking dai tokens', async() => {
+            let result
+
+            //check balance before staking
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('100'), 'investor dai token balance not correct before staking')
+
+            //check approval and staking
+            await daiToken.approve(dexquisiteCorpse.address, tokens('100'), {from: investor})
+            await dexquisiteCorpse.stake(tokens('100'), {from: investor})
+
+            //check wallet balance of investor
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), '0', 'investor dai balance not correct after staking')
+
+            //check that staking balance is correct
+            result = await dexquisiteCorpse.stakingBalance(investor)
+            assert.equal(result.toString(), tokens('100'), 'investor staking balance not correct after staking')
+        
+            //issue tokens
+            await dexquisiteCorpse.issueTokens({from: owner})
+
+            //check balance after issuance
+            result = await dexquisiteToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('100'), 'staking rewards balance not correct after issuance')
+
+            //ensure only owner can issue tokens
+            await dexquisiteCorpse.issueTokens({from: investor}).should.be.rejected
         })
     })
 
